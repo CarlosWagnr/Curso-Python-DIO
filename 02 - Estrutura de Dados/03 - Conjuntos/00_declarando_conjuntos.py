fed0ab53e80163b7ba6f49097972eb3a1f17a212
Carlos Wagner <carlos.alves.conceicao@hotmail.com>
2023-09-14 18:27:00 -0300

    Estudos sobre Conjuntos e Dicionarios de Dados em Python

---
+++ 02 - Estrutura de Dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -0,0 +1,10 @@
+# Sets são uma coleção que não possui objetos repetidos, 
+# usamos sets para representar os conjuntos matemáticos ou eliminar itens duplicados de um iterável
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
---
+++ 02 - Estrutura de Dados/03 - Conjuntos/05_difference.py
@@ -0,0 +1,8 @@
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.difference(conjunto_b) # Diferença de A para B
+print(resultado)
+
+resultado = conjunto_b.difference(conjunto_a) # Diferença de B para A
+print(resultado)
